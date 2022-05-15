7c73d561bc48b70affd1a74dc65310c23fde99fb
swapnilwaghmare24 <iamswapnilengg@gmail.com>
2022-05-15 22:33:17 +0530

    AddLast committed

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -3,9 +3,13 @@ package LinkedList;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList();
-        linkedList.push(70);
-        linkedList.push(30);
-        linkedList.push(56);
+        //linkedList.push(70);
+        //linkedList.push(30);
+        //linkedList.push(56);
+        // linkedList.print();
+        linkedList.addNodeLast(56);
+        linkedList.addNodeLast(30);
+        linkedList.addNodeLast(70);
         linkedList.print();
     }
 }
